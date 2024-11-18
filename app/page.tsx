09ab3f15c6039c86f10bb681e7609a3ba2832d29
Vidotto <vitotbrbr@gmail.com>
2024-11-18 13:27:04 -0300

    hotfix dynamic import

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,4 @@
+import dynamic from "next/dynamic";
 import AboutSection from "./components/AboutSection";
 import ContatoSection from "./components/ContatoSection";
 import FrontPage from "./components/FrontPage";
