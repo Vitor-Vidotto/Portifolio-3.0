cb22b8da316a7b49d9554d41c3e1124517424726
Vidotto <vitotbrbr@gmail.com>
2024-11-27 12:03:59 -0300

    css update

--- app/components/FrontPage.tsx
+++ app/components/FrontPage.tsx
@@ -14,13 +14,9 @@ export default function FrontPage() {
       <Navbar />
       <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
         <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-          <Image
-            src="/Vitor Vidotto.png"
-            alt="Vitor Vidotto Logo"
-            width={320}
-            height={48}
-            priority
-            />
+        <h1 className="text-5xl sm:text-6xl font-extrabold text-gradient animate-fadeIn">
+              Vitor Vidotto
+            </h1>
           <ol className="text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
             <li className="mb-2">
               <TypewriterLoop />
